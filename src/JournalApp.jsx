fc14f60978b85d71e22a7c07c598cd4f98cdc1a2
carloscarrete <carretecarlosg@gmail.com>
2022-08-03 12:46:38 -0500

    store configured

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './router/AppRouter'
+import { store } from './store'
 import { AppTheme } from './theme/AppTheme'
+
 
 const JournalApp = () => {
   return (
-    <BrowserRouter>
-      <AppTheme>
-        <AppRouter />
-      </AppTheme>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppTheme>
+          <AppRouter />
+        </AppTheme>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
